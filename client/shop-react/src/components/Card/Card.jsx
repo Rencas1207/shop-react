8be900ea3e2df56387d1a0d21ee7d76d0a130a54
Rencas <renzo.c@ohecommerce.agency>
2023-05-10 02:42:51 -0500

    shop-react conecction with strapi

--- client/shop-react/src/components/Card/Card.jsx
+++ client/shop-react/src/components/Card/Card.jsx
@@ -1,19 +1,27 @@
 import './Card.scss';
 import { Link } from 'react-router-dom';
 
-export function Card({ id, title, img, img2, isNew, oldPrice, price }) {
+export function Card({ item }) {
    return (
-      <Link className='link' to={`/product/${id}`}>
+      <Link className='link' to={`/product/${item?.attributes.id}`}>
          <div className="card">
             <div className="image">
-               {isNew && <span>New Season</span>}
-               <img src={img} alt={title} className='mainImg' />
-               <img src={img2} alt={title} className='secondImg' />
+               {item?.attributes.isNew && <span>New Season</span>}
+               <img
+                  src={import.meta.env.VITE_UPLOAD_URL + item?.attributes?.img?.data?.attributes?.url}
+                  alt={item?.attributes.title}
+                  className='mainImg'
+               />
+               <img
+                  src={import.meta.env.VITE_UPLOAD_URL + item?.attributes?.img2?.data?.attributes?.url}
+                  alt={item?.attributes.title}
+                  className='secondImg'
+               />
             </div>
-            <h2>{title}</h2>
+            <h2>{item?.attributes.title}</h2>
             <div className="prices">
-               <h3>${oldPrice}</h3>
-               <h3>${price}</h3>
+               <h3>${item?.attributes.oldPrice || item?.attributes.price + 20}</h3>
+               <h3>${item?.attributes.price}</h3>
             </div>
          </div>
       </Link>
